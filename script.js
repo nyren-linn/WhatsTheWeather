2cc2b6242d6182eb769d2057e15fa5ac57f2b66c
nyren-linn <56875885+nyren-linn@users.noreply.github.com>
2020-05-12 22:45:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+new Vue({
+  el: '#app',
+  data: {
+    forecasts:[],
+    pos:[{name: "Göteborg", lng:16.158, lat:58.5812}, {name:"Stockholm", lng:18.0649, lat:59.33258}],
+    myPos:null
+  },
+  created(){
+    for(let i=0; i<this.pos.length; i++){
+      var url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/"+this.pos[i].lng+"/lat/"+this.pos[i].lat+"/data.json";
+      fetch(url)
+      .then(response => response.json())
+      .then(result => {
+          let obj={
+            name:this.pos[i].name,
+            reports:result.timeSeries
+          }
+          this.forecasts.push(obj)
+      })
+    }
+  }
+})
